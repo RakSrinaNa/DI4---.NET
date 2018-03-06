6877f252ce45466db279b386ec0c049f98591fcf
NoahReyson <shadow.reyson@gmail.com>
2018-03-06 17:38:12 +0100

    FileDialog

---
+++ ProjetNET/Mercure/MainWindow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetNET
+{
+    public partial class MainWindow : Form
+    {
+        private String XMLFilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void SelectionXMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog MyFileDialog = new OpenFileDialog();
+            MyFileDialog.Filter = "XML Files|*.xml";
+            DialogResult HasClickedOK = MyFileDialog.ShowDialog();
+            if (HasClickedOK == DialogResult.OK)
+            {
+                XMLFilePath = MyFileDialog.FileName;
+                ToolStripStatusLabel1.Text = "";
+            }
+        }
+
+        private void IntegrationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (XMLFilePath != null)
+            {
+                ToolStripProgressBar1.Value = 0;
+                while (ToolStripProgressBar1.Value < 100)
+                {
+                    ToolStripProgressBar1.Value++;
+                    System.Threading.Thread.Sleep(10);
+                }
+            }
+            else
+            {
+                ToolStripStatusLabel1.Text = "Please select a file bro/biatch!";
+            }
+        }
+    }
+}
