ac8af2734c2ced97edbda815be302bb82326ed5a
Unknown <clement.0310@free.fr>
2018-03-27 15:37:10 +0200

    articles
    lol

---
+++ ProjetNET/Mercure/AddArticle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetNET
+{
+    public partial class AddArticle : Form
+    {
+        public AddArticle()
+        {
+            InitializeComponent();
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        public Article GetArticle()
+        {
+            Article Art = new Article();
+            Art.Reference = TextBoxReference.Text;
+            Art.Description = TextBoxDescription.Text;
+            Art.Brand = ComboBoxBrand.SelectedItem;
+            Art.Family = ComboBoxFamily.SelectedItem;
+            Art.SubFamily = ComboBoxSubFamily.SelectedItem;
+            bool ok;
+            float Price;
+            ok = float.TryParse(TextBoxPrice.Text, out Price);
+            if (!ok)
+                return null;
+            Art.Price = Price;
+            return Art;
+        }
+
+        public void SetArticle(Article Art)
+        {
+            TextBoxReference.Text = Art.Reference;
+            TextBoxDescription.Text = Art.Description;
+            Art.Brand = ComboBoxBrand.SelectedItem;
+            Art.Family = ComboBoxFamily.SelectedItem;
+            Art.SubFamily = ComboBoxSubFamily.SelectedItem;
+            TextBoxPrice.Text = Art.Price.ToString();
+            return Art;
+        }
+
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
