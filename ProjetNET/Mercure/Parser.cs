be08b2e788c51e1117b0ef59ac3e5c68ddcbf9af
Thomas Couchoud <zerderr@gmail.com>
2018-04-18 22:24:00 +0200

    Parser OK#14#15

--- ProjetNET/Mercure/Parser.cs
+++ ProjetNET/Mercure/Parser.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml;
+﻿using System.Xml;
 
 namespace ProjetNET
 {
     /// <summary>
     /// Parser from a XML file
     /// </summary>
-    class Parser
+    internal class Parser
     {
         /// <summary>
         /// Get the content of a XML file
         /// </summary>
         /// <param name="FileName">The path to the file to parse</param>
         /// <returns>The XmlDocument object containing the data</returns>
-        public static XmlDocument ParseXML(String FileName)
+        public static XmlDocument ParseXml(string FileName)
         {
             XmlDocument Xml = new XmlDocument();
             Xml.Load(FileName);
